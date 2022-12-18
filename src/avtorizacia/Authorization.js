f70d9ff3a6e8ecc553032e68251e2e90980329c6
kostyalm <113437523+kostyalm@users.noreply.github.com>
2022-12-18 22:27:52 +0500

    изменение формы регистрации

--- src/avtorizacia/Authorization.js
+++ src/avtorizacia/Authorization.js
@@ -1,72 +1,133 @@
-import React from 'react';
+import React from "react";
 
-
-class Authorization extends React.Component{
-    render(){
-      return( 
-<div className='authorization'>
-	<div class="section">
-		<div class="container">
-			<div class="row full-height justify-content-center">
-				<div class="col-12 text-center align-self-center py-5">
-					<div class="section pb-5 pt-5 pt-sm-2 text-center" align="center">
-						<h6 class="mb-0 pb-3"><span>Авторизоваться </span><span>Зарегистрироваться</span></h6>
-			          	<input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
-			          	<label for="reg-log"></label>
-						<div class="card-3d-wrap mx-auto">
-							<div class="card-3d-wrapper">
-								<div class="card-front">
-									<div class="center-wrap">
-										<div class="section text-center">
-											<h4 class="mb">Авторизоваться</h4>
-											<div class="form-group">
-												<input type="email" name="logemail" class="form-style" placeholder="Ваш адрес электронной почты" id="logemail" autocomplete="off"/>
-												<i class="input-icon uil uil-at"></i>
-											</div>	
-											<div class="form-group mt-2">
-												<input type="password" name="logpass" class="form-style" placeholder="Ваш пароль" id="logpass" autocomplete="off"/>
-												<i class="input-icon uil uil-lock-alt"></i>
-											</div>
-											<a href="#" class="btn mt-4">Авторизоваться</a>
-                            				<p class="mb-0 mt-4 text-center"><a href="#0" class="link">Забыли свой пароль?</a></p>
-				      					</div>
-			      					</div>
-			      				</div>
-								<div class="card-back">
-									<div class="center-wrap">
-										<div class="section text-center">
-											<h4 class="mb-4 pb-3">Зарегистрироваться</h4>
-											<div class="form-group">
-												<input type="text" name="logname" class="form-style" placeholder="Ваше имя" id="logname" autocomplete="off"/>
-												<i class="input-icon uil uil-user"></i>
-											</div>	
-											<div class="form-group mt-2">
-												<input type="email" name="logemail" class="form-style" placeholder="Ваш адрес электронной почты" id="logemail" autocomplete="off"/>
-												<i class="input-icon uil uil-at"></i>
-											</div>	
-											<div class="form-group mt-2">
-												<input type="password" name="logpass" class="form-style" placeholder="Ваш пароль" id="logpass" autocomplete="off"/>
-												<i class="input-icon uil uil-lock-alt"></i>
-											</div>
-											<div class="form-group mt-2">
-												<input type="password" name="logpass" class="form-style" placeholder="Ваш пароль" id="logpass" autocomplete="off"/>
-												<i class="input-icon uil uil-lock-alt"></i>
-											</div>
-												<div className="button">
-											<a href="#" class="btn mt-4">Регистрация</a>
-												</div>
-				      					</div>
-			      					</div>
-			      				</div>
-			      			</div>
-			      		</div>
-			      	</div>
-		      	</div>
-	      	</div>
-	    </div>
-	</div>
-    </div>
+class Authorization extends React.Component {
+  render() {
+    return (
+      <div className="authorization">
+        <div class="section">
+          <div class="container">
+            <div class="row full-height justify-content-center">
+              <div class="col-12 text-center align-self-center py-5">
+                <div
+                  class="section pb-5 pt-5 pt-sm-2 text-center"
+                  align="center"
+                >
+                  <h6 class="mb-0 pb-3">
+                    <span>Авторизоваться </span>
+                    <span>Зарегистрироваться</span>
+                  </h6>
+                  <input
+                    class="checkbox"
+                    type="checkbox"
+                    id="reg-log"
+                    name="reg-log"
+                  />
+                  <label for="reg-log"></label>
+                  <div class="card-3d-wrap mx-auto">
+                    <div class="card-3d-wrapper">
+                      <div class="card-front">
+                        <div class="center-wrap">
+                          <div class="section text-center">
+                            <h4 class="mb">Авторизоваться</h4>
+                            <div class="form-group">
+                              <input
+                                type="email"
+                                name="logemail"
+                                class="form-style"
+                                placeholder="Ваш адрес электронной почты"
+                                id="logemail"
+                                autocomplete="off"
+                              />
+                              <i class="input-icon uil uil-at"></i>
+                            </div>
+                            <div class="form-group mt-2">
+                              <input
+                                type="password"
+                                name="logpass"
+                                class="form-style"
+                                placeholder="Ваш пароль"
+                                id="logpass"
+                                autocomplete="off"
+                              />
+                              <i class="input-icon uil uil-lock-alt"></i>
+                            </div>
+                            <a href="#" class="btn mt-4">
+                              Авторизоваться
+                            </a>
+                            <p class="mb-0 mt-4 text-center">
+                              <a href="#0" class="link">
+                                Забыли свой пароль?
+                              </a>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="card-back">
+                        <div class="center-wrap">
+                          <div class="section text-center">
+                            <h4 class="mb-4 pb-3">Зарегистрироваться</h4>
+                            <div class="form-group">
+                              <input
+                                type="text"
+                                name="logname"
+                                class="form-style"
+                                placeholder="Имя"
+                                id="logname"
+                                autocomplete="off"
+                              />
+                              <i class="input-icon uil uil-user"></i>
+                            </div>
+                            <div class="form-group mt-2">
+                              <input
+                                type="email"
+                                name="logemail"
+                                class="form-style"
+                                placeholder="Адрес электронной почты"
+                                id="logemail"
+                                autocomplete="off"
+                              />
+                              <i class="input-icon uil uil-at"></i>
+                            </div>
+                            <div class="form-group mt-2">
+                              <input
+                                type="password"
+                                name="logpass"
+                                class="form-style"
+                                placeholder="Пароль"
+                                id="logpass"
+                                autocomplete="off"
+                              />
+                              <i class="input-icon uil uil-lock-alt"></i>
+                            </div>
+                            <div class="form-group mt-2">
+                              <input
+                                type="password"
+                                name="logpass"
+                                class="form-style"
+                                placeholder="Подтвердите пароль"
+                                id="logpass"
+                                autocomplete="off"
+                              />
+                              <i class="input-icon uil uil-lock-alt"></i>
+                            </div>
+                            <div className="button">
+                              <a href="#" class="btn mt-4">
+                                Регистрация
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
-}}
+  }
+}
 
 export default Authorization;
