1aab45a1afeeb298836d49f007add51f786c1fc7
kostyalm <113437523+kostyalm@users.noreply.github.com>
2023-04-14 19:18:52 +0500

    expert account adding phraseological

--- src/Activity_moderator_and_expert/Activity_exp.js
+++ src/Activity_moderator_and_expert/Activity_exp.js
@@ -1,76 +1,72 @@
-import React from 'react';
+import React from "react";
 import { useEffect, useState } from "react";
-import { Layout, Space, message, notification } from 'antd';
+import { Layout, Space, message, notification } from "antd";
 import { useNavigate } from "react-router-dom";
-import { Button, Form, Input, Checkbox, Select, Table } from 'antd';
-import { supabase } from '../supabaseClient.js';
-import Item from 'antd/es/list/Item.js';
-
+import { Button, Form, Input, Checkbox, Select, Table } from "antd";
+import { supabase } from "../supabaseClient.js";
+import Item from "antd/es/list/Item.js";
 
 const columns = [
   {
-    title: 'Номер запроса',
-    dataIndex: 'request_id',
-    key: 'request_id'
+    title: "Номер запроса",
+    dataIndex: "request_id",
+    key: "request_id",
   },
   {
-    title: 'Пользователь',
-    dataIndex: 'user_id',
-    key: 'user_id'
+    title: "Пользователь",
+    dataIndex: "user_id",
+    key: "user_id",
   },
   {
-    title: 'Русский перевод',
-    dataIndex: 'rus_request',
-    key: 'rus_request'
+    title: "Русский перевод",
+    dataIndex: "rus_request",
+    key: "rus_request",
   },
   {
-    title: 'Французский перевод',
-    dataIndex: 'fre_request',
-    key: 'fre_request'
+    title: "Французский перевод",
+    dataIndex: "fre_request",
+    key: "fre_request",
   },
   {
-    title: 'Корейский перевод',
-    dataIndex: 'kor_request',
-    key: 'kor_request'
+    title: "Корейский перевод",
+    dataIndex: "kor_request",
+    key: "kor_request",
   },
   {
-    title: 'Статус запроса',
-    dataIndex: 'status_id',
-    key: 'status_id'
+    title: "Статус запроса",
+    dataIndex: "status_id",
+    key: "status_id",
   },
   {
-    title: 'Вид операции',
-    dataIndex: 'type_id',
-    key: 'type_id'
-  }
-]
+    title: "Вид операции",
+    dataIndex: "type_id",
+    key: "type_id",
+  },
+];
 
 const GridDataOption = {
   rowCount: 10,
   page: 1,
-  orderBy: 'request_id',
-  from: 'request'
-}
+  orderBy: "request_id",
+  from: "request",
+};
 
 function update() {
   window.location.reload();
 }
-
-
 
 export default function Activity_moderator() {
   const [request, setrequest] = useState([]);
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
 
   const onSelectChange = (newSelectedRowKeys) => {
-    console.log('selectedRowKeys changed: ', newSelectedRowKeys);
+    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
     setSelectedRowKeys(newSelectedRowKeys);
-
   };
 
   const rowSelection = {
     selectedRowKeys,
-    onChange: onSelectChange
+    onChange: onSelectChange,
   };
 
   useEffect(() => {
@@ -83,27 +79,29 @@ export default function Activity_moderator() {
     const request = await supabase.from("request").select();
     const req = (await request).data;
     const data = await supabase
-      .from('request')
+      .from("request")
       .select()
-      .eq('status_id', `${4}`)
-    setrequest(data.data)
+      .eq("status_id", `${4}`);
+    setrequest(data.data);
   }
 
   async function delete_row() {
     for (let i = 0; i < selectedRowKeys.length; i++) {
       try {
         const { error } = await supabase
-          .from('request')
+          .from("request")
           .delete()
-          .eq('request_id', selectedRowKeys.at(i));
-        console.log("Запись удалена", selectedRowKeys.at(i))
-      }
-      catch (error) {
-        notification.open({ message: 'Ошибка', description: 'Ошибка,некоректно введены данные' });
+          .eq("request_id", selectedRowKeys.at(i));
+        console.log("Запись удалена", selectedRowKeys.at(i));
+      } catch (error) {
+        notification.open({
+          message: "Ошибка",
+          description: "Ошибка,некоректно введены данные",
+        });
       }
     }
-    getrequest()
-    update()
+    getrequest();
+    update();
   }
 
   async function add_request() {
@@ -112,42 +110,62 @@ export default function Activity_moderator() {
 
     for (let i = 0; i < selectedRowKeys.length; i++) {
       try {
-        const phrase = adde[selectedRowKeys.at(i)]
-        // console.log(adde[selectedRowKeys.at(i)].data["request_id"])
-        console.log(phrase.data)
-        // const { error } = await supabase
-        //   .from('phraseological')
-        //   .insert({ phrase_id: '4' })
-        // console.log("Запись добавленна")
-
-      }
-      catch (error) {
-        notification.open({ message: 'Ошибка', description: error.message });
+        //получаем выбранную фразу(ы)
+        const phrase = await supabase
+          .from("request")
+          .select()
+          .eq("request_id", selectedRowKeys.at(i));
+        console.log(phrase.data[0]['request_id']) //обращение к полю возвращаемого объекта из таблицы
+//------------------------------------------------------------------------------------------------------------
+        //Обновляем поле update_at
+        var update1 = ((new Date()).toISOString()).toLocaleString();
+        const { error1 } = await supabase
+          .from('request')
+          .update({status_id:'3',update_at:(update1)})
+          .eq('request_id',selectedRowKeys.at(i));
+//------------------------------------------------------------------------------------------------------------
+        //Добавляем одобренный запрос в таблицу с фразеологизмами
+        const { error } = await supabase
+          .from('phraseological')
+          .insert({ phrase_id: selectedRowKeys.at(i), rus: phrase.data[0]['rus_request'], fre: phrase.data[0]['fre_request'], kor: phrase.data[0]['kor_request'],  Subject_id: 1})
+//------------------------------------------------------------------------------------------------------------
+        console.log("Запись добавленна")
+      } catch (error) {
+        notification.open({ message: "Ошибка", description: error.message });
       }
     }
-
+    update()
   }
-
 
   const navigate = useNavigate();
   return (
-    <div className='Activ_moder'>
-      <div style={{ position: 'relative', left: '63%' }}>
-        <Button onClick={delete_row} className='btn-7'>Удалить</Button>
-        <Button onClick={update} className='btn-7'>Обновить</Button>
-        <Button onClick={add_request} className='btn-7'>Добавить запись</Button>
-        <Button onClick={() => { navigate("/Moderator_personal_account") }} className='btn-7'>Назад</Button>
+    <div className="Activ_moder">
+      <div style={{ position: "relative", left: "63%" }}>
+        <Button onClick={delete_row} className="btn-7">
+          Удалить
+        </Button>
+        <Button onClick={update} className="btn-7">
+          Обновить
+        </Button>
+        <Button onClick={add_request} className="btn-7">
+          Добавить запись
+        </Button>
+        <Button
+          onClick={() => {
+            navigate("/Moderator_personal_account");
+          }}
+          className="btn-7"
+        >
+          Назад
+        </Button>
       </div>
       <Table
-
         dataSource={request}
         columns={columns}
         rowSelection={rowSelection}
         rowKey={(record) => record.request_id}
         onRow={(record) => ({
-          onClick: () => {
-
-          },
+          onClick: () => {},
         })}
       />
     </div>
